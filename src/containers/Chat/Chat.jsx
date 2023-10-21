c5ccecd1b07bd05b7a75946eae47dcc02cf8ece8
Susros <kelvin.susros@gmail.com>
2023-10-21 11:47:10 +1100

    tailwind

---
+++ src/containers/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div className='text-white'>Chat</div>
+  );
+};
+
+export default Chat;
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import Popup from './Popup';
 import './index.css';
+import '../../containers/style.css';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
