75169a13d42e22efee3eef8a78ae2609a57c53f1
Mas Abdi <iamasoudabdi@gmail.com>
2023-10-21 11:54:19 +1100

    added openai in background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,52 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+import secrets from 'secrets';
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'CALL_OPENAI') {
+    callOpenAI(message.prompt).then(sendResponse);
+    return true; // Indicates that the response will be sent asynchronously.
+  }
+});
+
+async function callOpenAI(prompt) {
+  console.log('callOpenAI', prompt);
+
+  const OPENAI_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+
+  const headers = {
+    Authorization: `Bearer ${secrets.API_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  const body = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'Your a helpful assistant that is completion my sentences for me.',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    max_tokens: 50,
+  };
+
+  try {
+    const response = await fetch(OPENAI_ENDPOINT, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+    console.log('OpenAI response', data);
+    return (
+      (data.choices && data.choices[0] && data.choices[0].message.content) || ''
+    );
+  } catch (error) {
+    console.error('Error calling OpenAI:', error);
+    return 'Error';
+  }
+}
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -4,9 +4,51 @@ import Chat from '../../containers/Chat/Chat';
 import './Popup.css';
 
 const Popup = () => {
+  const [number, setNumber] = React.useState(0);
+
+  // document
+  //   .getElementById('exampleButton')
+  //   .addEventListener('click', async () => {
+  //     const text = document.getElementById('inputText').value;
+  //     const response = await chrome.runtime.sendMessage({
+  //       type: 'CALL_OPENAI',
+  //       prompt: text,
+  //     });
+  //     alert(response);
+  //   });
+
+  const root = document.getElementById('root');
+
+  const onButtonClick = async () => {
+    console.log('calling openAI');
+    setNumber(number + 1);
+    chrome.runtime.sendMessage({
+      type: 'CALL_OPENAI',
+      prompt: 'hey',
+    });
+    console.log(root);
+  };
+
   return (
     <div className="App">
-      <Chat />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!
+        </a>
+
+        <button className="bg-red-500" onClick={onButtonClick}>
+          Click me! {number}
+        </button>
+      </header>
     </div>
   );
 };
